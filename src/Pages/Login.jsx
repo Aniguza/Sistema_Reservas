1a1810df2e7c78c1ee6785ceda480757e18d9746
Patty <patriciacv2253@gmail.com>
2025-10-20 23:44:29 -0500

    Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import '../App.css'
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+      <fieldset className="login-card">
+        <h1 className="login-title">¡BIENVENIDO A LAB RESERVE DE UTP!</h1>
+
+        <p className="login-subtitle">
+          Inicie sesión para administrar sus reservas de equipos de laboratorio
+        </p>
+
+        <form className="login-form">
+          <input
+            type="email"
+            className="login-input top"
+            placeholder="Correo institucional"
+          />
+          <input
+            type="password"
+            className="login-input bottom"
+            placeholder="Contraseña"
+          />
+
+          <button className="login-button">Iniciar sesión</button>
+        </form>
+        <p className="login-footer">
+          ¿No tienes cuenta?{' '}
+          <a href="/register" className="login-register-link">
+            Regístrate
+          </a>
+        </p>
+      </fieldset>
+    </div>
+  );
+};
